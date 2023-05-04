07af50d04d218d647a6b379ede3f3b816d1a4e37
kudashov <kudashov@surf.dev>
2023-05-04 14:04:12 +0300

    Изменил настройки темы

--- app/src/main/java/com/kudashov/learning_compose/ui/theme/Color.kt
+++ app/src/main/java/com/kudashov/learning_compose/ui/theme/Color.kt
@@ -2,7 +2,11 @@ package com.kudashov.learning_compose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Pink80 = Color(0xFFEFB8C8)
+val Pink40 = Color(0xFF7D5260)
+
+val White = Color(0xFFFFFFFF)
+val LightGrey = Color(0xFFF2F2F2)
+
+val Dark = Color(0xFF251F26)
+val Black = Color(0xFF000000)
