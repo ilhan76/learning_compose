b7f7fdc01cd0592493fea167c3c8951df6503986
kudashov <kudashov@surf.dev>
2023-05-07 10:11:51 +0300

    Добавил шрифты

--- app/src/main/java/com/kudashov/learning_compose/ui/theme/Color.kt
+++ app/src/main/java/com/kudashov/learning_compose/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.kudashov.learning_compose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+//region Theme
 val Pink80 = Color(0xFFEFB8C8)
 val Pink40 = Color(0xFF7D5260)
 
@@ -10,3 +11,12 @@ val LightGrey = Color(0xFFF2F2F2)
 
 val Dark = Color(0xFF251F26)
 val Black = Color(0xFF000000)
+//endregion
+
+//region Text
+val NewFeatureTextColor = Color(0xFF30C150)
+val StandardTextColor = Color(0xFF251F26)
+val HintTextColor = Color(0xA3251F26)
+val LightTextColor = Color(0x52251F26)
+//endregion
+
